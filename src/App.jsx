a62833ed30fadae407e5e15a0758dcfb176764dd
Clemente  Silva vicuña <clementes@MacBook-Pro.local>
2024-05-06 14:07:44 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "@nextui-org/react";
+function App() {
+  return (
+    <>
+      <h1 className="text bg-red-700"> hola</h1>
+      <Button color="primary">Button</Button>
+    </>
+  );
+}
+
+export default App;
