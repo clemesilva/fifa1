a63832b9fe3d61485e6a0217dc4f79fe6dd6dac5
Clemente  Silva vicuña <clementes@MacBook-Pro.local>
2024-05-06 14:43:27 -0400

    tercera

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import { Button } from "@nextui-org/react";
 function App() {
   return (
     <>
-      <h1 className="text bg-red-700"> hola</h1>
-      <Button color="primary">Button</Button>
-      <h1 className="text bottom-2 bg-slate-500">goola</h1>{" "}
+      <h1 className="text bg-red-700"> chupalo</h1>
+      <Button color="primary">nico</Button>
+      <h1 className="text bottom-2 bg-slate-500">qlao</h1>{" "}
     </>
   );
 }
